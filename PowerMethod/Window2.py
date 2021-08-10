bef4f7273d56e17e95af8348a7491c24c1207898
jordán sanchez <jordandavid.sanchez@alumnos.ulagos.cl>
2021-08-10 00:19:20 -0400

    se agrego square matrix size a Window2

--- PowerMethod/Window2.py
+++ PowerMethod/Window2.py
@@ -20,5 +20,6 @@ tab1_layout = [[sg.Menu(menu_def, tearoff=True)],
     [sg.Radio('Random Stochastic Matrix     ', "key1", default=True, size=(20,1)), sg.Radio('Comma-Separated Values (CSV) File ', "key1")]], title='Select the matrix source', relief=sg.RELIEF_SUNKEN)],
     [sg.Text('_' * 80)],
     [sg.Text('Number of Iterations: ', size=(15, 1)),  sg.Input(key='txt1', size=(3,1))],
+    [sg.Text('Square Matrix Size: ', size=(15, 1)),  sg.Input(key='txt2', size=(3,1))],
 
 ]
